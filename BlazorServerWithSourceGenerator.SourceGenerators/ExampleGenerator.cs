b85b4265cb5799af1e1526c98b7dc499ca173152
irwinj <irwinj@southern.edu>
2021-08-18 00:10:45 -0400

    Add project files.

---
+++ BlazorServerWithSourceGenerator.SourceGenerators/ExampleGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace BlazorServerWithSourceGenerator.SourceGenerators;
+
+[Generator]
+public class ExampleGenerator : ISourceGenerator
+{
+	public void Execute(GeneratorExecutionContext context)
+	{
+		if(!Debugger.IsAttached)
+			Debugger.Launch();
+
+		var json = JsonSerializer.Serialize(new { Hello = "World" });
+		var text = $@"
+namespace Hello;
+
+public class World
+{{
+	public static string Message() => ""Hello World!"";
+	public static string JsonString() => @""{json.Replace('"', '`')}"";
+}}
+";
+
+		context.AddSource("HelloWorld.cs", SourceText.From(text, Encoding.UTF8));
+	}
+
+	public void Initialize(GeneratorInitializationContext context)
+	{
+	}
+}
